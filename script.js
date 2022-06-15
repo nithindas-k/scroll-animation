7e94896e12e5980d99ce14d48cf2eef98b34d318
Nithin <nithindaskavungal@gmail.com>
2022-06-15 23:53:23 +0530

    scroll animation

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
-const boxes = document.querySelectorAll('.box')
+const boxes = document.querySelectorAll(".box");
 
-window.addEventListener('scroll', chechbox)
+window.addEventListener("scroll", checkBox);
 
-checkBoxes()
+checkBox();
 
+function checkBox() {
+  const triggerbottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box, index) => {
+    const cardNumber = index + 1;
+    const boxTop = box.getBoundingClientRect().top;
+    if (boxTop < triggerbottom) {
+      box.style.fontSize = "100px";
+      if (cardNumber % 6 == 1) {
+        box.innerText = "s";
+        box.style.color = "#363062";
+        box.style.backgroundColor = "#E9D5CA";
+      }
+      if (cardNumber % 6 == 2) {
+        box.innerText = "a";
+        box.style.color = "#C2DED1";
+        box.style.backgroundColor = "#354259";
+      }
+      if (cardNumber % 6 == 3) {
+        box.innerText = "y";
+        box.style.color = "#242F9B";
+        box.style.backgroundColor = "#DBDFFD";
+      }
+      if (cardNumber % 6 == 4) {
+        box.innerText = "o";
+        box.style.color = "#FFE3A9";
+        box.style.backgroundColor = "#FF5D5D";
+      }
+      if (cardNumber % 6 == 5) {
+        box.innerText = "o";
+        box.style.color = "#371B58";
+        box.style.backgroundColor = "#7858A6";
+      }
+      if (cardNumber % 6 == 0) {
+        box.innerText = "j";
+        box.style.color = "#121212";
+        box.style.backgroundColor = "#E0DECA";
+      }
 
-function chechbox() {
- 
- 
-    const triggerbottom =  window.innerHeight / 5 * 4
-boxes.forEach(box  => { 
-    const boxTop = box.getBoundingClientRect().top
-
-    if(boxTop < triggerbottom) {
-        box.classList.add('show')
-
-
+      box.classList.add("show");
     } else {
-        box.classList.remove('show')
+      box.classList.remove("show");
     }
-})
-
+  });
 }
